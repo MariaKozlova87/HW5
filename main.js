de667ee0dac828159f8f78240a86df1218bc0d2b
MariaKozlova87 <64326665+MariaKozlova87@users.noreply.github.com>
2020-05-26 22:14:13 +0300

    Delete main.js

--- main.js
+++
@@ -1,8 +0,0 @@
-const formEl = document.forms['article-create'];
-formEl.addEventListener ('submit', (ev) => {
-    ev.preventDefault();
-    const formData = new FormData(ev.target);
-    axios.post('/articles/create', formData);
-    
-   
-});
